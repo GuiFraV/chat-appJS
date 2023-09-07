7b5cd95d2d67f448efa1fbd528d211027d47f661
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-07 19:41:54 +0200

    add cors & socket.io

--- server/index.js
+++ server/index.js
@@ -3,12 +3,30 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const http = require("http").Server(app);
+const cors = require("cors");
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+})
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!}`);
+    socket.on('disconnect', () => {
+        console.log(`⚡: ${socket.id} user just disconnected!}`);
+    }
+})
+
+application.use(cors());
+
 app.get("/api", (req, res) => {
     res.json({
         message: "Hello world",
     });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
