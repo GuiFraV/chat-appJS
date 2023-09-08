a5aa7bda6f2f13b5ca806231f1d45f201ed2027b
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-08 11:34:46 +0200

    server with react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import socketIO from "socket.io-client";
+const socket = socketIO.connect("http://localhost:4000");
+
 function App() {
-  return (
-      <div>
-          <p>Hello World!</p>
-      </div>
-  );
+    return (
+        <div>
+            <p>Hello World!</p>
+        </div>
+    );
 }
+
+export default App;
