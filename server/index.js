6dc5a0c9d5e2db2806aceb79f19dc88b7f8403da
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-08 12:43:21 +0200

    .env file remove from tracking

--- server/index.js
+++ server/index.js
@@ -8,24 +8,30 @@ const cors = require("cors");
 app.use(cors());
 
 const socketIO = require("socket.io")(http, {
-    cors: {
-        origin: "http://localhost:5173",
-    },
+	cors: {
+		origin: "http://localhost:5173",
+	},
 });
 
 socketIO.on("connection", (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on("disconnect", () => {
-        console.log("🔥: A user disconnected");
-    });
+	console.log(`⚡: ${socket.id} user just connected!`);
+
+	socket.on("message", (data) => {
+		console.log(data);
+		socketIO.emit("messageResponse", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("🔥: A user disconnected");
+	});
 });
 
 app.get("/api", (req, res) => {
-    res.json({
-        message: "Hello world",
-    });
+	res.json({
+		message: "Hello world",
+	});
 });
 
 http.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
